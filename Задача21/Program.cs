7eb6aec152d9045497441c4a54dd128b2b646aa0
Kostas <langas.kostya@mail.ru>
2022-08-03 16:49:01 +0500

    нахождение расстояния между 2 точками координат

---
+++ Задача21/Program.cs
@@ -0,0 +1,10 @@
+﻿// Напишите программу, 
+//которая принимает на вход координаты двух точек и находит расстояние между ними в 2D пространстве.
+
+int x1 = Convert.ToInt32(Console.ReadLine());
+int y1 = Convert.ToInt32(Console.ReadLine());
+int x2 = Convert.ToInt32(Console.ReadLine());
+int y2 = Convert.ToInt32(Console.ReadLine());
+int stepen = 2;
+double n = Math.Sqrt((Math.Pow(x2-x1, stepen)) + (Math.Pow(y2-y1, stepen)));
+Console.WriteLine(n);
