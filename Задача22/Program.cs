f5c95fcf28c846c0ac5d4a236d7a27508c4663be
Kostas <langas.kostya@mail.ru>
2022-08-03 17:13:54 +0500

    нахождение квадрата от 1 до заданного числа

---
+++ Задача22/Program.cs
@@ -0,0 +1,13 @@
+﻿// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу квадратов чисел от 1 до N.
+
+Console.WriteLine("Введите число");
+int n = Convert.ToInt32(Console.ReadLine());
+
+int i = 1;
+
+while (i <= n )
+{
+    Console.WriteLine(Math.Pow(i,2));
+    i++;
+
+}
