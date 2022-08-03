4cb013e0010e0fe2ae5910fa2ed9b6097b359ec2
Kostas <langas.kostya@mail.ru>
2022-08-03 16:10:26 +0500

    Обратная задача17

---
+++ Задача18/Program.cs
@@ -0,0 +1,18 @@
+﻿//Напишите программу, которая по заданному номеру четверти, 
+//показывает диапазон возможных координат точек в этой четверти (x и y).
+
+Console.WriteLine("Введите номер четверти");
+int n = Convert.ToInt32(Console.ReadLine());
+while (n < 1 || n > 4)
+{
+    Console.WriteLine("Ошибка, введите номер четверти");
+    n = Convert.ToInt32(Console.ReadLine());
+}
+if (n == 1)
+    Console.WriteLine("x > 0, y > 0");
+if (n == 2)
+    Console.WriteLine("x < 0 , y > 0");
+if (n == 3)
+    Console.WriteLine("x < 0, y < 0");
+if (n == 4)
+    Console.WriteLine("x > 0, y < 0");
